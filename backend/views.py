ec60bde9fc96ae40af1dba25b1ed5daac65fed90
Joachim <joachim.schmidt@wp.pl>
2020-04-26 22:25:09 +0200

    added actual events loading

--- backend/views.py
+++ backend/views.py
@@ -5,17 +5,19 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.shortcuts import redirect
-
-
 from django import template
 
 from django.db import transaction
+
+from backend.models import Event
+
 register = template.Library()
 
 
-
 def index(request):
-    return render(request, 'backend/index.html')
+    events = Event.objects.all()
+    return render(request, 'backend/index.html', {'all_events_info': events})
+
 
 def my_logout(request):
     logout(request)
