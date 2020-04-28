ddbe0bdf68c190546ad1f106d40ad48ad6408d7e
Artur Pazurkiewicz <pazurkiewiczartur@gmail.com>
2020-04-28 13:05:03 +0200

    added initial data

--- initdb.sh
+++ initdb.sh
@@ -4,5 +4,5 @@ rm db.sqlite3
 rm -r /backend/migrations/*
 python3 manage.py makemigrations backend
 python3 manage.py migrate
-python3 manage.py createsuperuser
+python3 manage.py loaddata initial_data.json
 python3 manage.py runserver
