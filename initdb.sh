efaa7bc60ed3c1f9470655ed6f8c6a53abf59467
Artur Pazurkiewicz <pazurkiewiczartur@gmail.com>
2020-05-24 13:03:51 +0200

    fixed initdb.sh

--- initdb.sh
+++ initdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm db.sqlite3
-rm -r /backend/migrations/*
+rm -r backend/migrations/*
 python manage.py makemigrations backend
 python manage.py migrate
 python manage.py loaddata initial_data.json
